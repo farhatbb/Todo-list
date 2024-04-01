6aa4e618307f892c3efe2dd0870456abb5a48f31
farhatbb <Farhatbashir77@yahoo.com>
2024-04-01 23:07:41 +0500

    Todo-list

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let Add = await inquirer.prompt([
+        { message: "What you want add in your todos", type: "input", name: "todo" },
+        { message: "Do you want to add some more items", type: "confirm", name: "confirm", default: "false" },
+    ]);
+    todos.push(Add.todo);
+    condition = Add.confirm;
+    console.log(todos);
+}
